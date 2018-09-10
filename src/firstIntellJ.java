7e7d640b36162f5ff68977a065e72ba20f76c621
David Hopkins <t00197124@365s.ittralee.ie>
2018-09-10 17:25:47 +0100

    Initial commit

---
+++ src/firstIntellJ.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+public class firstIntellJ {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("David Hopkins");
+        JOptionPane.showMessageDialog(null,"Welcome"+ name );
+
+
+
+    }
+
+
+        {
+
+
+    }
+}
